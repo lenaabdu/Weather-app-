4284be186b0de4666ebae363827b74f65d4117b4
lenaabdu <lenaabdu23@gmail.com>
2021-09-09 17:08:25 +0100

    Updates

---
+++ precache-manifest.95df2695f1d8874bcd508c1ee60f71ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a408fb61f4538b4ab361c3890202554",
+    "url": "/Weather-app-React-/index.html"
+  },
+  {
+    "revision": "fa502915dc34ae12ab29",
+    "url": "/Weather-app-React-/static/css/main.b3f1c802.chunk.css"
+  },
+  {
+    "revision": "86bcb8d7980f4ea8e35f",
+    "url": "/Weather-app-React-/static/js/2.d1946e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app-React-/static/js/2.d1946e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa502915dc34ae12ab29",
+    "url": "/Weather-app-React-/static/js/main.e35a38d8.chunk.js"
+  },
+  {
+    "revision": "17ccc34f01ee886274e0",
+    "url": "/Weather-app-React-/static/js/runtime-main.dbcf24ee.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather-app-React-/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "e4f249151da95c454bad44df0a20e974",
+    "url": "/Weather-app-React-/static/media/background2.e4f24915.jpg"
+  },
+  {
+    "revision": "7fc288eaeecfece8997f27f22e7d9ec6",
+    "url": "/Weather-app-React-/static/media/city3.7fc288ea.jpg"
+  }
+]);
